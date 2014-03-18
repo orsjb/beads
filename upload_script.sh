70aae7782ea7fac50777658ad026313d6b428649
Oliver Bown <ollie@icarus.nu>
2014-03-18 15:24:10 +1100

    Chmod on upload_script.sh. Tested.

--- upload_script.sh
+++ upload_script.sh
@@ -4,3 +4,4 @@ cd build/beads_processing
 zip -r beads.zip beads
 scp beads.zip orsjb@beadsproject.net:beadsproject.net/library/
 scp beads/library.properties orsjb@beadsproject.net:beadsproject.net/library/beads.txt
+cd ../..
