b3b858c07e1c0744ef75de4195fe423c849f056d
Ollie Bown <ollie@icarus.nu>
2021-11-04 12:06:53 +1100

    Added temporary TestJack.java class to check AudioServer.Jack is working (broke on Apple M1, macOS 12.0.1). JNA was not finding the Jack library. Solved by pointing JNA to /usr/local/lib (done via code, as in the TestJack code). Also noted that, I think, the brew install of Jack was not built for the M1 architecture, but the installer from the jackaudio website, which installs to /usr/local/lib does work. Haven't actually confirmed this last detail.

--- src/beads_io/java/TestJack.java
+++ src/beads_io/java/TestJack.java
@@ -1,15 +1,34 @@
 import net.beadsproject.beads.core.AudioContext;
 import net.beadsproject.beads.core.AudioIO;
+import net.beadsproject.beads.core.IOAudioFormat;
 import net.beadsproject.beads.data.Buffer;
 import net.beadsproject.beads.ugens.Gain;
 import net.beadsproject.beads.ugens.WavePlayer;
 import org.jaudiolibs.beads.AudioServerIO;
 
+import java.io.File;
+
 public class TestJack {
 
+    //Essential: must tell JNA where Jack lives. This might vary from system to system.
+    //example on current config (Mac M1), Jack installed via downloaded package:
+    //Add VM arg -Djna.library.path="/usr/local/lib" when running
+    //or, use the first bit of code below to do this in the running program
+
     public static void main(String[] args) {
+
+
+        //code required to set up correct path for Jack library
+        String libPath = "/usr/local/lib";
+        String jnaPath = System.getProperty("jna.library.path", "").trim();
+        if (jnaPath.isEmpty()) {
+            System.setProperty("jna.library.path", libPath);
+        } else {
+            System.setProperty("jna.library.path", jnaPath + File.pathSeparator + libPath);
+        }
+
         AudioIO io = new AudioServerIO.Jack();
-        AudioContext ac = new AudioContext(io);
+        AudioContext ac = new AudioContext(io, 256, new IOAudioFormat(44100, 16, 1, 2));
         WavePlayer wp = new WavePlayer(ac,500, Buffer.SINE);
         Gain g = new Gain(ac, 1, 0.1f);
         g.addInput(wp);
